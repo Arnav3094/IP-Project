00edac759314e5b1e6a8ede02c4d637f9944ce67
Arnav Mangla <arnav3094pvt@gmail.com>
2022-03-03 20:27:44 +0530

    added release_date.py

---
+++ release_date.py
@@ -0,0 +1,14 @@
+from create import *
+import create
+
+df = create.df
+mean_df = create.mean_df
+df_album_release_date_sorted = df.sort_values(by=['album_release_date'])
+
+py.hist(df_album_release_date_sorted.album_release_date.str.slice(0, 4).astype(int), bins=[x for x in range(1955, 2030, 5)], rwidth=0.9, color='#997BFF')
+py.xticks(range(1955, 2030, 5), rotation=25)
+py.ylabel('Number of Songs')
+py.xlabel('Year of Release')
+py.grid()
+py.savefig('release_date_hist_year')
+py.close()
