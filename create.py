14d82d85e0120461a82d4da2210721d5e5d43fb8
Arnav Mangla <arnav3094pvt@gmail.com>
2022-03-03 21:45:00 +0530

    updated list of artists

--- create.py
+++ create.py
@@ -20,11 +20,11 @@ client_id = '602ac06174344597a70ddd9949e97f50'
 client_secret = '286a7572b3e944ff9708690eaaa4b8f2'
 
 artists = []
-artists += list(map(str, 'The Weeknd,Kanye West,Drake,Joyner,Lucas,Eminem,J. Cole,Kendrick Lamar,Logic,Meek Mill,50 Cent,Rihanna,Nicki Minaj,Cardi B,Travis Scott,Juice WRLD,Post Malone,Machine Gun Kelly,Closed on Sunday,Powfu,Kid Cudi,A$AP Rocky,Dr. Dre,2pac,The Notorious B.I.G.,JAY-Z,Pusha-T,Joey Bada$$,Hopsin,XXXTENTACION'.split(',')))
+artists += list(map(str, 'The Weeknd,Kanye West,Drake,Joyner Lucas,Eminem,J. Cole,Kendrick Lamar,Logic,Meek Mill,50 Cent,Rihanna,Nicki Minaj,Cardi B,Travis Scott,Juice WRLD,Post Malone,Machine Gun Kelly,Closed on Sunday,Powfu,Kid Cudi,A$AP Rocky,Dr. Dre,2pac,The Notorious B.I.G.,JAY-Z,Pusha-T,Joey Bada$$,Hopsin,XXXTENTACION'.split(',')))
 artists += list(map(str, 'Queen,The Beatles,Michael Jackson,Glass Animals,Taylor Swift,Imagine Dragons,Coldplay,Halsey,X Ambassadors,Hozier,The Chainsmokers,Avicii,Dj Snake,Martin Garrix,Skrillex,20syl,Kygo'.split(',')))
-artists += list(map(str, 'Ed Sheeran,Bastille,Billie Eilish,Duncan Laurence,Dempsey Hope,Christian French,Miley Cyrus,Frank Ocean,Ellie Goulding,Camila Cabello,Alec Benjamin,Bruno Mars,Lauv,Selena Gomez,Maroon 5,Jon Bellion,JP Saxe,Alicia Keys,Ariana Grande,Justin Bieber,Anson Seabra,Justin Timberlake,Khalid,6LACK,Dean Lewis'.split(',')))
+artists += list(map(str, 'Ed Sheeran,Bastille,Billie Eilish,Duncan Laurence,Dempsey Hope,Christian French,Miley Cyrus,Frank Ocean,Ellie Goulding,Camila Cabello,Alec Benjamin,Bruno Mars,Lauv,Selena Gomez,Maroon 5,Jon Bellion,JP Saxe,Ariana Grande,Justin Bieber,Anson Seabra,Justin Timberlake,Khalid,6LACK,Dean Lewis'.split(',')))
 artists += list(map(str, 'One Direction,Katy Perry,Lady Gaga,Beyonce,Shawn Mendes,Sam Smith,Mac Miller,Linkin Park,Britney Spears,Alicia Keys,Black Eyed Peas,Green Day,Denzel Curry,AURORA,SZA,Lukas Graham'.split(',')))
-artists += list(map(str, 'Bon Jovi,Sting,Backstreet Boys,Whitney Houston,David Bowie,AC/DC,Elvis Presley,Elton John,John Lennon,Bob Marley,ABBA,Paul McCartney'.split(',')))
+artists += list(map(str, 'Bon Jovi,Sting,Backstreet Boys,Whitney Houston,David Bowie,AC/DC,Elvis Presley,Elton John,John Lennon,Bob Marley,ABBA,Paul McCartney,Adele,Dua Lipa'.split(',')))
 
 if __name__ == '__main__':
     spotify = FeaturesSpotifyAPI(client_id, client_secret, artists)
