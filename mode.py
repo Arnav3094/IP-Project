d7c19e80a77f74b139400881ba90cb1b8beef34d
Arnav Mangla <arnav3094pvt@gmail.com>
2022-03-08 12:25:26 +0530

    added mode.py

---
+++ mode.py
@@ -0,0 +1,14 @@
+import create
+from create import *
+
+df = create.df
+df_sorted_mode = df.sort_values(by=['mode'], ascending=True)
+
+py.figure(dpi=300)
+x = range(0, 2)
+mode_counts = [list(df_sorted_mode['mode']).count(i) for i in x]
+py.pie(mode_counts, labels=x)
+py.legend(title='Mode Value:')
+py.title("Distribution of Modes")
+py.savefig('mode_pie')
+py.close()
